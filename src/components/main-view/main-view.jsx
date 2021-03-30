aceffc168c103fc32bb209c4b440563015f3a898
EhWanu <77857074+EhWanu@users.noreply.github.com>
2021-03-30 11:20:32 -0400

    3.5 submission

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -5,6 +5,8 @@ import axios from 'axios';
 import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export class MainView extends React.Component {
 
@@ -61,9 +63,20 @@ export class MainView extends React.Component {
     if (movies.length === 0) return <div className="main-view"/>
 
     return (
-      <div className="main-view">
-        {movies.map(movie => <MovieCard key={movie._id} movie={movie} onMovieClick={newSelectedMovie => { this.setState({ selectedMovie: newSelectedMovie }); }} />)}
-      </div>
+      <Row className="main-view justify-content-md-center container-fluid d-flex">
+        {selectedMovie
+          ? (
+            <Col md={4}>
+              <MovieView movie={selectedMovie} onBackClick={movie => this.onMovieClick(null)} />
+            </Col>
+          )
+          : movies.map(movie => (
+            <Col md={3}>
+              <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)}/>
+            </Col>
+          ))
+        }
+      </Row>
     );
   }
 }
